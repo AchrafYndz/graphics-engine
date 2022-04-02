81ad8f7b0c36ef60fb49b6e37b100160ddeb16c2
YAchrafY <achraf.yandouzi@gmail.com>
2022-04-02 17:23:29 +0200

    Write a part of the draw_zbuf_trag() function

--- src/include/ZBufferTriangles.cc
+++ src/include/ZBufferTriangles.cc
@@ -1,10 +1,14 @@
+#include <cmath>
+#include <algorithm>
+#include <limits>
+
 #include "ZBufferTriangles.h"
 #include "Logic3D.h"
 
 std::vector<Face> triangulate(const Face &face) {
     std::vector<Face> triangles;
-    for (int i=1; i<=face.point_indexes.size(); i++) {
-        triangles.emplace_back(std::vector<int> {0, i, i+1});
+    for (int i = 1; i <= face.point_indexes.size(); i++) {
+        triangles.emplace_back(std::vector<int>{0, i, i + 1});
     }
     return triangles;
 }
@@ -17,7 +21,25 @@ draw_zbuf_trag(ZBuffer &zbuffer, img::EasyImage &img, Vector3D const &A, Vector3
     Point2D BProjected = doProjection(B, d);
     Point2D CProjected = doProjection(C, d);
     // Determine which pixels belong to the triangle
-    int y_min = ;
-    int y_max = ;
+    int y_min = round(std::min({AProjected.y, BProjected.y, CProjected.y}) + 0.5);
+    int y_max = round(std::max({AProjected.y, BProjected.y, CProjected.y}) - 0.5);
+
+    int x_L_AB = std::numeric_limits<int>::max();
+    int x_L_AC = std::numeric_limits<int>::max();
+    int x_L_BC = std::numeric_limits<int>::max();
+
+    int x_R_AB = -std::numeric_limits<int>::max();
+    int x_R_AC = -std::numeric_limits<int>::max();
+    int x_R_BC = -std::numeric_limits<int>::max();
+
+    for (int y = y_min; y <= y_max; y++) {
+        int x_L = round(std::min({x_L_AB, x_L_AC, x_L_BC}) + 0.5);
+        int x_R = round(std::max({x_R_AB, x_R_AC, x_R_BC}) - 0.5);
+    }
     // Calculate the 1/z value
+    double x_G = (AProjected.x + BProjected.x + CProjected.x)/3;
+    double y_G = (AProjected.y + BProjected.y + CProjected.y)/3;
+    double z_GInverse = 1/3*A.z + 1/3*B.z + 1/3*C.z;
+
+
 }
